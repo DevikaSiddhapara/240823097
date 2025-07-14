81051477bbb4b7a8939e85a52920daddca16df83
DevikaSiddhapara <devikasiddhapara@gmail.com>
2025-07-14 10:55:11 +0530

    practice of create the node project and run

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("hello world")
+
+const name="devika"
+console.log(name)
+
+const num=7
+if(num%2==0){
+    console.log("Number is even")
+}
+else{
+    console.log("Number is odd")
+}
+
+const a=
