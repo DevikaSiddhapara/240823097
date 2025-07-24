a08c26ae4f93419674f3668cb2bf4db50e6d073d
DevikaSiddhapara <devikasiddhapara@gmail.com>
2025-07-24 08:16:34 +0530

    read and write file

--- fs.js
+++ fs.js
@@ -1,10 +1,30 @@
-const path=require("path");
-const moment=require("moment");
+// const path=require("path");
+// const moment=require("moment");
 
-console.log(path.dirname(__filename));
-console.log(path.extname("myfile.txt"));
+// console.log(path.dirname(__filename));
+// console.log(path.extname("myfile.txt"));
 
-console.log(moment().format("DD-MM-YYYY     HH:mm:ss"));
+// console.log(moment().format("DD-MM-YYYY     HH:mm:ss"));
 
-console.log(moment());
+// console.log(moment());
+
+const fs=require('fs');
+let mydata="Hello ,Good morning";
+fs.writeFile('mydata.txt',mydata,(err)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("file created successfully");
+        fs.readFile('mydata.txt','utf8',(err,data)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log("file Content : "+data);
+            }
+        })
+    }
+})
+
 
